1dc6d27f4e1ab0ba85590cfc5629285c28e6aa6e
lucas-mateus <xlucasmat@gmail.com>
2021-07-07 04:15:11 -0300

    criando hook para sala

--- src/pages/Room.tsx
+++ src/pages/Room.tsx
@@ -1,4 +1,3 @@
-import { useEffect } from 'react'
 import { useState, FormEvent } from 'react'
 import toast, { Toaster } from 'react-hot-toast'
 import { useParams } from 'react-router-dom'
@@ -7,6 +6,7 @@ import { Button } from '../components/Button'
 import { Question } from '../components/Question'
 import { RoomCode } from '../components/RoomCode'
 import { useAuth } from '../hooks/useAuth'
+import { useRoom } from '../hooks/useRoom'
 import { database } from '../services/firebase'
 import '../styles/room.scss'
 
@@ -15,58 +15,14 @@ type RoomParam = {
     id: string
 }
 
-type FirebaseQuestions = Record<string, {
-    author: {
-        name: string,
-        avatar: string
-    },
-    content: string,
-    isHighlighted: boolean,
-    isAnswered: boolean
-}>
-
-type QuestionType = {
-    id: string,
-    author: {
-        name: string,
-        avatar: string
-    },
-    content: string,
-    isHighlighted: boolean,
-    isAnswered: boolean
-
-}
-
 export function Room() {
+
     const { user } = useAuth();
     const [newQuestion, setNewQuestion] = useState('');
     const params = useParams<RoomParam>();
     const roomId = params.id;
-    const [title, setTitle] = useState('');
-    const [questions, setQuestions] = useState<QuestionType[]>([]);
 
-
-    useEffect(() => {
-        const roomRef = database.ref(`rooms/${roomId}`);
-
-        roomRef.on('value', room => {
-            const databaseRoom = room.val();
-            const firebaseQuestions: FirebaseQuestions = databaseRoom.questions ?? {}
-
-            const parsedQuestions = Object.entries(firebaseQuestions).map(([key, value]) => {
-                return {
-                    id: key,
-                    content: value.content,
-                    author: value.author,
-                    isHighlighted: value.isHighlighted,
-                    isAnswered: value.isAnswered
-                }
-            })
-
-            setTitle(databaseRoom.title)
-            setQuestions(parsedQuestions);
-        })
-    }, [roomId]);
+    const {title,questions} = useRoom(roomId);
 
     async function handleSendQuestion(event: FormEvent) {
         event.preventDefault();
